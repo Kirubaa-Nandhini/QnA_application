e87f50252a363ee6f0deb2d99672f9e99597752c
Kirubaa-Nandhini <kirubaanandhini@testpress.in>
2026-03-04 17:02:52 +0530

    Revert accidental authentication changes

--- accounts/views.py
+++ accounts/views.py
@@ -6,8 +6,6 @@ from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.views.generic import CreateView, DetailView
 from django.urls import reverse_lazy
-from django.db import transaction
-from django.http import HttpResponseNotAllowed
 from .forms import SignupForm
 from .models import Profile
 
@@ -16,7 +14,6 @@ class SignupView(CreateView):
     template_name = 'accounts/signup.html'
     success_url = reverse_lazy('home')
 
-    @transaction.atomic
     def form_valid(self, form):
         response = super().form_valid(form)
         # Create profile for new user
@@ -35,17 +32,16 @@ class ProfileView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Real statistics for questions and answers
-        context['questions_count'] = self.request.user.questions.count()
-        # Answers count will be updated once Answer model is implemented
+        # Placeholder stats (Questions/Answers count)
+        # These can be updated once Question/Answer models are implemented
+        context['questions_count'] = getattr(self.request.user, 'questions', None).count() if hasattr(self.request.user, 'questions') else 0
         context['answers_count'] = getattr(self.request.user, 'answers', None).count() if hasattr(self.request.user, 'answers') else 0
         return context
 
 @login_required
 def logout_user(request):
-    if request.method != 'POST':
-        return HttpResponseNotAllowed(['POST'])
-    logout(request)
+    if request.method == 'POST':
+        logout(request)
     return redirect('home')
 
 class PasswordChangeManualView(PasswordChangeView):
